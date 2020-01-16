1e74fc77b6e5784613fc0a1ba2390763e9e85ccc
Chuck Francis <bnsf5622@gmail.com>
2020-01-15 21:19:15 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace guessMeMore1._1.sln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random guess = new Random(44);
+
+            int guessresult = guess.Next(100) - 1;
+
+            Console.WriteLine(" I have chosen a number can you guess it?");
+
+            else if (x == 3)
+
+            { 
+                (guessresult == 20)
+            {
+                Console.WriteLine("44 is Not it");
+
+            
+            }
+
+        }
+
+    }
+}
